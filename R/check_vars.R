78924ba8c5df19a7737eeee4c4289dd300ffb1b5
‘gertstulp’ <g.stulp@rug.nl>
2018-01-24 10:21:41 +0100

    R-folder

---
+++ R/check_vars.R
@@ -0,0 +1,8 @@
+check_vars <- function(vector_vars = NULL) {
+  if(vector_vars = NULL) {
+    print("No variables specified")
+  } else {
+    print("Check whether all variables are numeric")
+  }
+
+}
